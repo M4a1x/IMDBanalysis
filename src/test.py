f2070f40780635eb283f1a40303db96b4c982903
Max Stabel <dev@stabel.family>
2017-06-06 02:26:41 +0900

    Added some tests. This now takes around 5GB of RAM on my pc

--- src/test.py
+++ src/test.py
@@ -3,10 +3,15 @@ import time
 from sys import getsizeof
 from extract import *
 
+BASE_PATH = os.path.abspath('../database/selection/')
+PICKLE_PATH = os.path.abspath('../database/pickle/')
 RATINGS_PATH = os.path.abspath('../database/selection/ratings.list')
 GENRES_PATH = os.path.abspath('../database/selection/genres.list')
 KEYWORDS_PATH = os.path.abspath('../database/selection/keywords.list')
 LANGUAGES_PATH = os.path.abspath('../database/selection/language.list')
+LOCATIONS_PATH = os.path.join(BASE_PATH, 'locations.list')
+RUNNING_TIMES_PATH = os.path.join(BASE_PATH, 'running-times.list')
+TECHNICALS_PATH = os.path.join(BASE_PATH, 'technical.list')
 
 
 def test_extract_function(function, arg):
@@ -21,7 +26,7 @@ def test_extract_function(function, arg):
     else:
         data = function(*arg)
     dattime = time.process_time() - starttime
-    
+
     print("{}: Size: {}. Length: {}. Time: {}.".format(
         function.__name__, getsizeof(data), len(data), dattime))
     print("{}: First Element: {}".format(function.__name__, data[0]))
@@ -30,19 +35,16 @@ def test_extract_function(function, arg):
 
 gg = test_extract_function(get_genres, GENRES_PATH)
 gk = test_extract_function(get_keywords, KEYWORDS_PATH)
-gl =  test_extract_function(get_languages, LANGUAGES_PATH)
-gr =  test_extract_function(get_ratings, RATINGS_PATH)
-test_extract_function(combine_lists, (gg, gk, gl, gr))
+glang =  test_extract_function(get_languages, LANGUAGES_PATH)
+grat =  test_extract_function(get_ratings, RATINGS_PATH)
+gloc = test_extract_function(get_locations, LOCATIONS_PATH)
+grun = test_extract_function(get_running_times, RUNNING_TIMES_PATH)
+gt = test_extract_function(get_technicals, TECHNICALS_PATH)
+combine = test_extract_function(combine_lists, (gg, gk, gl, gr))
 
+starttime = time.process_time()
+save(PICKLE_PATH, genres=gg, keywords=gk, language=glang, ratings=grat, locations=gloc, running_times=grun, technical=gt)
+gg, gk, glang, grat, gloc, grun, gt = load(PICKLE_PATH, "genres", "keywords", "language", "ratings", "locations", "running_times", "technical")
+dattime = time.process_time() - starttime
 
-# list1_dict = dict(GR)
-# list2_dict = dict(GG)
-# list3_dict = dict(GK)
-# # Create sets from the KEYS of the dicts and
-# # get a set which contains all entries from both ('&' operator).
-# # Then loop over the set and use entries as keys
-# # for the dicts to create the list
-# starttime = time.process_time()
-# combined =  [(id, list1_dict[id], list2_dict[id], list3_dict[id])
-#             for id in set(list1_dict) & set(list2_dict) & set(list3_dict)]
-# combinedtime = time.process_time() - starttime
+combine = test_extract_function(combine_lists, (gg, gk, glang, grat, gloc, grun, gt))
